fa5360a6b64925e5c38b95338693650f753f0fe6
Book-0225 <97381093+Book-0225@users.noreply.github.com>
2026-08-09 17:41:35 +0900

    refactor: 色情報を共通で扱いやすいように

--- ToolAnalyzer.cpp
+++ ToolAnalyzer.cpp
@@ -1,26 +1,11 @@
-﻿#include "Eap2Common.h"
+﻿#include "AnalyzerColor.h"
+#include "Eap2Common.h"
 #include "Eap2Config.h"
 
 static constexpr UINT WM_PROGRESS = WM_USER + 1;
 static constexpr UINT WM_DONE = WM_USER + 2;
 static constexpr UINT WM_FRAME_CHANGE = WM_USER + 3;
 static constexpr UINT_PTR ANALYSIS_TIMER_ID = 1;
-
-static constexpr COLORREF C_BG = RGB(18, 18, 22);
-static constexpr COLORREF C_PANEL = RGB(26, 26, 34);
-static constexpr COLORREF C_CTRL = RGB(30, 30, 42);
-static constexpr COLORREF C_BORDER = RGB(44, 44, 58);
-static constexpr COLORREF C_TEXT = RGB(216, 216, 230);
-static constexpr COLORREF C_LABEL = RGB(110, 110, 134);
-static constexpr COLORREF C_GREEN = RGB(60, 200, 100);
-static constexpr COLORREF C_YELLOW = RGB(224, 192, 44);
-static constexpr COLORREF C_RED = RGB(220, 56, 48);
-static constexpr COLORREF C_BLUE = RGB(60, 148, 220);
-static constexpr COLORREF C_ORANGE = RGB(220, 128, 50);
-static constexpr COLORREF C_GRAPHBG = RGB(10, 10, 18);
-static constexpr COLORREF C_GRID = RGB(32, 32, 44);
-static constexpr COLORREF C_GRIDLBL = RGB(56, 56, 70);
-static constexpr COLORREF C_PROGFG = RGB(44, 100, 190);
 
 struct Preset {
     const wchar_t* name;
